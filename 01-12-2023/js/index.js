e3c0efa6714fe7b5c3b9e9dcb35a805c9f2ac8b1
ziaul sarker <ziaulsarker@gmail.com>
2023-12-02 00:26:37 -0500

    day 1 part 2

--- 01-12-2023/js/index.js
+++ 01-12-2023/js/index.js
@@ -1,3 +1,4 @@
+// Part 1;
 // The newly-improved calibration document consists of lines of text; each 
 // line originally contained a specific calibration value that the Elves now 
 // need to recover. On each line, the calibration value can be found by combining 
@@ -14,12 +15,39 @@
 
 // In this example, the calibration values of these four lines are 12, 38, 15, and 77. Adding these together produces 142.
 
-// Part 1;
+// Part 2;
+
+/* Your calculation isn't quite right. It looks like some of the digits are actually spelled out with letters: one, two, three, four, five, six, seven, eight, and nine also count as valid "digits".
+
+Equipped with this new information, you now need to find the real first and last digit on each line. For example:
+
+two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen
+In this example, the calibration values are 29, 83, 13, 24, 42, 14, and 76. Adding these together produces 281.
+
+What is the sum of all of the calibration values?
+
+*/
 
 
 import * as fs from 'node:fs/promises';
-import path from 'node:path';
 
+const mapper = {
+  'one': 1,
+  "two": 2,
+  "three": 3,
+  "four": 4,
+  "five": 5,
+  "six": 6,
+  "seven": 7,
+  "eight": 8,
+  "nine": 9
+}
 
 // returns the file handler
 const createFileHandler = async pathToFile => {
@@ -56,7 +84,36 @@ const sumTwoNumbers = arrOfNumbers => arrOfNumbers.reduce((acc, curr) => parseIn
 
 const extractNumsFromLine = line => {
   const characters = line.split("");
-  return characters.filter(char => char.match(/^\d+$/));
+  const result = [];
+
+
+  let count = 0;
+
+  while (count < characters.length) {
+    let char = characters[count];
+    const isNumber = !isNaN(parseInt(char))
+    if(isNumber) {
+      result.push(parseInt(char));
+      count++;
+      continue;
+    }
+    let combined = char;
+
+    for(let j = count + 1; j < characters.length; j++) {
+      combined += characters[j];
+
+      if(mapper[combined]) {
+        result.push(mapper[combined]);
+        count = j;
+        break;
+      }
+    }
+
+    count++;
+  }
+
+  return result;
+
 }
 
 const getDigitsFromLine = digitsInLine => {
@@ -66,6 +123,7 @@ const getDigitsFromLine = digitsInLine => {
 
   if(digitsInLine.length === 1) return [firstDigit, firstDigit]
 
+  console.log({digits: [firstDigit, digitsInLine.at(-1)]})
   return [firstDigit, digitsInLine.at(-1)]
 }
 
